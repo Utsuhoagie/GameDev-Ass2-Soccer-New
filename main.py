8ee20c61508bd3269b8da82f8216c12c94e00bf5
UtsuhoagieRyeuji <thiendangssb@gmail.com>
2021-09-26 16:10:23 +0700

    Change main.py

--- main.py
+++ main.py
@@ -1,4 +1,145 @@
-from TimerColHandlers import *
+from Objects.Player import *
+from Objects.Ball import *
+from Objects.Field import *
+from Objects.Score import *
+
+
+# ------ Game global variables -----------------------------
+
+# NOTE: Passing in (x,y) coordinates in pm.Space() <=> (0,0) is bottom left
+# So Pymunk's physics are upside down, but Pygame still blits it correctly
+
+ball = Ball(MIDX, MIDY, B_VEL)
+
+score = ScoreController()
+
+p1_1 = Player(200, MIDY + 50, 1)
+p1_2 = Player(200, MIDY - 50, 1)
+
+p2_1 = Player(WIDTH - 200, MIDY + 50, 2)
+p2_2 = Player(WIDTH - 200, MIDY - 50, 2)
+
+
+wallL = Wall((BORDER, BORDER + MENU_HEIGHT), (BORDER, HEIGHT - BORDER), 1)
+wallR = Wall((WIDTH - BORDER, BORDER + MENU_HEIGHT), (WIDTH - BORDER, HEIGHT - BORDER), 2)
+wallU = Wall((BORDER, BORDER + MENU_HEIGHT), (WIDTH - BORDER, BORDER + MENU_HEIGHT), 3)
+wallD = Wall((BORDER, HEIGHT - BORDER), (WIDTH - BORDER, HEIGHT - BORDER), 4)
+
+goal1 = Goal((BORDER + 5, MIDY - 80), (BORDER + 5, MIDY + 80), 1)
+goal2 = Goal((WIDTH - BORDER - 5, MIDY - 80), (WIDTH - BORDER - 5, MIDY + 80), 2)
+
+
+pGroup = pg.sprite.Group(p1_1, p1_2, p2_1, p2_2)
+goalGroup = pg.sprite.Group(goal1, goal2)
+wallList = [wallL, wallR, wallU, wallD]
+
+
+# ----- Classes -------------------------------------
+
+class Timer:
+    def __init__(self):
+        self.timer = 0
+
+    def set(self, n: int):
+        self.timer = n
+
+    def timeUp(self) -> bool:
+        return self.timer == 0
+
+    def update(self):
+        if self.timer:
+            self.timer -= 1
+        elif self.timer == 0 and ball.shape.touchedGoal:
+            pg.event.post(pg.event.Event(EVENT_RESET))
+
+timer = Timer()
+
+
+
+# ----- CollisionHandler functions ---------------------------------------------------
+"""
+    When a collision happens, the corresponding pymunk.CollisionHandler for the 2 types of objects will call:
+        begin:      before collision happens
+                    do calculations/etc
+                    returns True if collide (aka bounce off), False if not (aka ignore collision)
+                    if returns False, separate() won't be called
+
+        (also call pre_solve, post_solve, but not used here)
+
+        separate:   after 2 objects are separated
+
+    Arbiter: basically contains info about the 2 colliding objects:
+        usually use arbiter.shapes to get the shapes of the objects
+            or arbiter.shapes[i].body to get the body of the i-th shape (i = 0 or 1)
+            etc
+        
+        check members of the classes in Objects files!!!
+"""
+
+
+def begin_PlayerWall(arbiter, space, data) -> bool:
+
+    wall = arbiter.shapes[1].id
+    pid = arbiter.shapes[0].id
+
+    for player in pGroup:    
+        if wall == 1 and player.shape.id == pid:
+            player.touchL = True
+        elif wall == 2 and player.shape.id == pid:
+            player.touchR = True
+        elif wall == 3 and player.shape.id == pid:
+            player.touchU = True
+        elif wall == 4 and player.shape.id == pid: 
+            player.touchD = True
+
+    return True
+
+def separate_PlayerWall(arbiter, space, data):
+    wall = arbiter.shapes[1].id
+    pid = arbiter.shapes[0].id
+
+    for player in pGroup:
+        if wall == 1 and player.shape.id == pid:
+            player.touchL = False
+        elif wall == 2 and player.shape.id == pid:
+            player.touchR = False
+        elif wall == 3 and player.shape.id == pid:
+            player.touchU = False
+        elif wall == 4 and player.shape.id == pid:
+            player.touchD = False
+
+
+def begin_BallGoal(arbiter, space, data) -> bool:
+
+    arbiter.shapes[0].touchedGoal = True
+    arbiter.shapes[0].body.velocity = arbiter.shapes[0].body.velocity.scale_to_length(55)
+
+    if arbiter.shapes[1].id == 1:
+        score.score_P2 += 1
+    else:
+        score.score_P1 += 1
+    
+    timer.set(90)   # pause for 1.5s
+
+    return False    # no collision
+
+    
+
+# NOTE: These begin functions will make the ball IGNORE all collisions after a goal
+#       until it resets (due to begin_BallGoal or pressing 'R')
+
+def begin_BallWall(arbiter, space, data) -> bool:
+    if arbiter.shapes[0].touchedGoal:
+        return False
+    else:
+        return True
+
+
+def begin_BallPlayer(arbiter, space, data) -> bool:
+    if arbiter.shapes[0].touchedGoal:
+        return False
+    else:
+        return True
 
 
 # ----- Main game functions ---------------------
